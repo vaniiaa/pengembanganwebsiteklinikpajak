e82018875c1f0316e9885496534df07c3be7d54a
vania <vanivania2005@gmail.com>
2024-05-13 11:29:33 +0700

    Pengerjaan tugas M8 a. pembuatan model laravel b. penggunaan model di controller c. mengatur route d. menampilkan view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListProductController;
+use App\Http\Controllers\ListProdukController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::get('/', function () {
 
 
 Route::get('/list_product', [ListProductController::class, 'tampilkan']);
+Route::get('/listproduk',  [ListProdukController::class, 'show']);
